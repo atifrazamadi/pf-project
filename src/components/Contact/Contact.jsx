a9461bc659c7a0f9cf0b1cbefac917ae0d509437
atifrazamadi <atifrazamadi@gmail.com>
2023-06-22 17:05:58 +0500

    firt commint

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,111 @@
+import React from "react";
+import { MdCall } from "react-icons/md";
+import { BsFillChatDotsFill } from "react-icons/bs";
+import { HiChatBubbleBottomCenter } from "react-icons/hi2";
+const Contact = () => {
+  return (
+    <section className="c-wrapper">
+      <div className="paddings innerWidth flexCenter c-container">
+        {/* left side */}
+        <div className="flexColStart c-left">
+          <span className="orangeText">Our Contact</span>
+          <span className="primaryText">Easy to Contact us</span>
+          <span className="secondaryText">
+            We always ready to help by providijng the best services for you. We
+            beleive a good blace to live can make your life better
+          </span>
+
+          <div className="flexColStart contactModes">
+            {/* first row */}
+            <div className="flexStart row">
+              {/* first mode */}
+              <div className="flexColCenter mode">
+                <div className="flexStart">
+                  <div className="flexxCenter icon">
+                    <MdCall size={25} />
+                  </div>
+                  <div className="flexColStart detail">
+                    <span className="primaryText">Call </span>
+                    <span className="secondaryText">012 123 456 789</span>
+                  </div>
+                </div>
+
+                <div className="flexCenter button">Call Now</div>
+              </div>
+
+
+              {/* second mode */}
+
+
+              <div className="flexColCenter mode">
+                <div className="flexStart">
+                  <div className="flexxCenter icon">
+                    <BsFillChatDotsFill size={25} />
+                  </div>
+                  <div className="flexColStart detail">
+                    <span className="primaryText">Chat </span>
+                    <span className="secondaryText">012 123 456 789</span>
+                  </div>
+                </div>
+
+                <div className="flexCenter button">Chat Now</div>
+              </div>
+
+            </div>
+
+
+
+            {/* second row */}
+
+            <div className="flexStart row">
+              {/* third mode */}
+              <div className="flexColCenter mode">
+                <div className="flexStart">
+                  <div className="flexCenter icon">
+                    <BsFillChatDotsFill size={25} />
+                  </div>
+                  <div className="flexColStart detail">
+                    <span className="primaryText">Video Call </span>
+                    <span className="secondaryText">012 123 456 789</span>
+                  </div>
+                </div>
+
+                <div className="flexCenter button"> Video Call Now</div>
+              </div>
+
+
+              {/* fourth mode */}
+
+
+              <div className="flexColCenter mode">
+                <div className="flexStart">
+                  <div className="flexxCenter icon">
+                    <HiChatBubbleBottomCenter size={25} />
+                  </div>
+                  <div className="flexColStart detail">
+                    <span className="primaryText">Messege </span>
+                    <span className="secondaryText">012 123 456 789</span>
+                  </div>
+                </div>
+
+                <div className="flexCenter button">Messege Now</div>
+              </div>
+
+            </div>
+            </div>
+          </div>
+        
+        {/* right side */}
+
+        <div className="c-right">
+          <div className="image-container">
+            <img src="./home1.jpg" alt="" />
+          </div>
+        </div>
+      </div>
+      
+    </section>
+  );
+};
+
+export default Contact;
